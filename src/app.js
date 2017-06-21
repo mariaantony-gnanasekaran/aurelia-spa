fd1c99805a06e57693a19e26055d976b6d569e15
mariaantony-gnanasekaran <mariaantony.gnanasekaran@syncfusion.com>
2017-06-21 17:27:27 +0530

    ss

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Aurelia';
     config.options.pushState = true;
-    config.options.root = '/';
+    config.options.root = '/aurelia-spa/';
     config.map([
       { route: ['', 'first'], name: 'first',      moduleId: 'first',      nav: true, title: 'first' },
       { route: 'second',  name: 'second', moduleId: 'second', nav: true, title: 'second' }
